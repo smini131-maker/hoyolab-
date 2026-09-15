476db1cb6ae0f029679c5b485ea952a8c72a2d8a
smini131-maker <smini131@naver.com>
2026-09-15 17:34:02 +0900

    chore: bump Galaxy attendance app to v1.1.0

--- GalaxyStoreAutoAttendance/app/build.gradle.kts
+++ GalaxyStoreAutoAttendance/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.smini.galaxyattendance"
         minSdk = 31
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.1.0"
     }
 
     buildTypes {
